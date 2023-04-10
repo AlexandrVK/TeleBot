688a0dededf550eef996eb0a547246902743a4d5
AlexandrVK <alexandrvk@gmail.com>
2023-04-10 17:56:53 +0300

    publish text to Telegram channel

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # fetch_spacex_images.fetch_spacex_last_launch()
     # fetch_nasa_images.fetch_nasa(10)
     # fetch_nasa_epic_images.fetch_nasa_epic()
-    # 5eb87d47ffd86e000604b38a
+
     #formatprint = pprint.PrettyPrinter()
     #formatprint.pprint(photos)
     print("Всё сделано")
