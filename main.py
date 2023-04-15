fbeb7a3fa08cd901cdae740b97b75f6df8daa2a5
AlexandrVK <alexandrvk@gmail.com>
2023-04-15 14:44:19 +0300

    Исправлены аргументы функций

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ import os
 def main():
 
     load_dotenv()
-    api_key =  os.environ['NASA_API_KEY']
+    api_key =  os.environ["NASA_API_KEY"]
     print("Качаем фото из spacex_last_launch")
-    fetch_spacex_images.fetch_spacex_last_launch(api_key)
+    fetch_spacex_images.fetch_spacex_last_launch()
     print("Качаем фото из NASA APOD")
-    fetch_nasa_apod_images.fetch_nasa(20)
+    fetch_nasa_apod_images.fetch_nasa(api_key,20)
     print("Качаем фото из NASA EPIC")
     fetch_nasa_epic_images.fetch_nasa_epic(api_key)
 
-    #formatprint = pprint.PrettyPrinter()
-    #formatprint.pprint(photos)
+   
+
     print("Всё сделано")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
