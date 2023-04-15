99515a4dcc562090a1d7ce17ae7a2341ab4f2956
AlexandrVK <alexandrvk@gmail.com>
2023-04-15 14:42:36 +0300

    Починены комментари и кавычки

--- public_random_photo.py
+++ public_random_photo.py
@@ -3,19 +3,19 @@ import random
 import argparse
 from dotenv import load_dotenv
 import telegram
-from my_modules import get_all_images,compress_image
+from modules import get_all_images,compress_image
 
 def main():
 
     load_dotenv()
 
-    chat_id = os.environ['CHAT_ID']
+    chat_id = os.environ["CHAT_ID"]
 
-    bot = telegram.Bot(token = os.environ['TOKEN'])
+    bot = telegram.Bot(token = os.environ["TOKEN"])
 
-    # Путь к каталогу с изображениями
+    
     images_directory = "images"
-    # Максимальный размер изображения в байтах (20 MB)
+    
     max_image_size_bytes = 20 * 1024 * 1024
 
 
@@ -24,7 +24,7 @@ def main():
     random.shuffle(image_files)
 
     parser = argparse.ArgumentParser(description="Публикация случайного или выбраного изображеня из каталога")
-    parser.add_argument("filename", nargs='?', default=os.path.basename(image_files[0]), help="Имя публикуемого файла ")
+    parser.add_argument("filename", nargs="?", default=os.path.basename(image_files[0]), help="Имя публикуемого файла ")
     args = parser.parse_args()
 
 
@@ -34,10 +34,10 @@ def main():
         if os.path.getsize(filepath) > max_image_size_bytes:
             compress_image(filepath, max_image_size_bytes)
 
-        with open(filepath, 'rb') as file:
+        with open(filepath, "rb") as file:
             bot.send_photo(chat_id=chat_id, photo=file)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
 
 
